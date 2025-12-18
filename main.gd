5c2442aa59916920e8a363a45f06756f8229b489
Mian-code <mpatw143@gmail.com>
2025-12-17 21:16:19 -0500

    init2

--- main.gd
+++ main.gd
@@ -5,3 +5,5 @@ var time_survived := 0.0
 func _process(delta):
 	if not get_tree().paused:
 		time_survived += delta
+		# Update the label text
+		$UI/TimeLabel.text = "Time: " + str(round(time_survived))
