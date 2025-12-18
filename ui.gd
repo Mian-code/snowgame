26a7843bd97dc7cd04a4b18ee4f7f3483caaff34
Mian-code <mpatw143@gmail.com>
2025-12-18 08:22:16 -0500

    final

--- ui.gd
+++ ui.gd
@@ -2,13 +2,21 @@ extends CanvasLayer
 
 var best_time := 0.0
 
+# Called from Castle when the game ends
 func show_loss(time_survived):
 	visible = true
+	
 	$LostLabel.text = "YOU LOST"
 	$TimeLabel.text = "Time: " + str(round(time_survived))
 	
 	if time_survived > best_time:
 		best_time = time_survived
 	
-	$BestLabel.text = "Best: " + str(round(best_time))
-	$UI/TimeLabel.text = "Time: " + str(round(time_survived))
+	
+	$RetryLabel.text = "Press R to Retry"
+
+# Handles retry input even when game is paused
+func _process(_delta):
+	if Input.is_action_just_pressed("restart"):
+		get_tree().paused = false
+		get_tree().reload_current_scene()
